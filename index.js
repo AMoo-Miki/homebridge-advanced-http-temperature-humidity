b26dfe07c082000ff6e0b13f35563d2adfe19156
Miki <amoo_miki@yahoo.com>
2017-11-02 21:53:08 -0700

    Add value modifiers and error handlers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     homebridge.registerAccessory("homebridge-advancedhttptemperaturehumidity", "AdvancedHttpTemperatureHumidity", AdvancedHttpTemperatureHumidity);
-}
+};
 
 function AdvancedHttpTemperatureHumidity(log, config) {
     this.log = log;
@@ -21,10 +21,15 @@ function AdvancedHttpTemperatureHumidity(log, config) {
     this.name = config["room"] || config["name"];
 
     this.manufacturer = config["manufacturer"] || "HttpTemperatureHumidity";
-    this.model = config["model"] || "Default";
+    this.model = config["model"] || "HTTP";
     this.serial = config["serial"] || "18981898";
 
     this.disableHumidity = config["humidity"] || false;
+
+    if (config.modifiers) {
+        if (typeof config.modifiers.temperature === 'string') this.temperatureModifier = new Function('x', 'return ' + config.modifiers.temperature);
+        if (typeof config.modifiers.humidity === 'string') this.humidityModifier = new Function('x', 'return ' + config.modifiers.humidity);
+    }
 }
 
 AdvancedHttpTemperatureHumidity.prototype = {
@@ -47,30 +52,78 @@ AdvancedHttpTemperatureHumidity.prototype = {
     },
 
     getStateHumidity: function (callback) {
-        callback(null, this.humidity);
+        var self = this;
+        setTimeout(function() {
+            if (self._updating) return self.getStateHumidity(callback);
+            if (self._hasErrors) return callback(self._hasErrors);
+            callback(null, self.humidity);
+        }, 1000);
     },
 
     getState: function (callback) {
+        this._updating = true;
+        this._hasErrors = null;
         this.httpRequest(this.url, "", "GET", this.username, this.password, this.sendimmediately, function (error, response, responseBody) {
-
             if (error) {
-                this.log('Get Temperature failed: %s', error.message);
-                callback(error);
-            } else {
-                this.log('Get Temperature succeeded!');
-                var info = JSON.parse(responseBody);
+                this._updating = false;
+                this._hasErrors = 'Get Temperature failed: ' + error.message;
+                this.log(this._hasErrors);
+                return callback(error);
+            }
 
-                var temperature = parseFloat(info.temperature);
+            if (response && response.statusCode !== 200) {
+                this._updating = false;
+                this._hasErrors = 'Get Temperature failed: ' + response.statusCode;
+                this.log(this._hasErrors);
+                return callback(this._hasErrors);
+            }
 
-                if (this.humidityService !== false) {
-                    var humidity = parseFloat(info.humidity)
+            var info;
+            try {
+                info = JSON.parse(responseBody);
+            } catch(ex) {
+                this._updating = false;
+                this._hasErrors = 'Get Temperature failed to parse: ' + responseBody;
+                this.log(this._hasErrors);
+                return callback(this._hasErrors);
+            }
+
+            if (info.status !== 1) {
+                this._updating = false;
+                this._hasErrors = 'Get Temperature failed to get valid values with status: ' + info.status;
+                this.log(this._hasErrors);
+                return callback(this._hasErrors);
+            }
+
+            this.log('Get Temperature succeeded!');
+
+            var temperature = parseFloat(info.temperature);
+            if (!isFinite(temperature)) {
+                this._updating = false;
+                this._hasErrors = 'Get Temperature failed with invalid temperature: ' + info.temperature;
+                this.log(this._hasErrors);
+                return callback(this._hasErrors);
+            }
+
+            if (this.temperatureModifier) temperature = this.temperatureModifier(temperature);
+
+            if (this.humidityService !== false) {
+                var humidity = parseFloat(info.humidity);
+                if (!isFinite(humidity)) {
+                    this._hasErrors = 'Get Temperature failed with invalid humidity: ' + info.humidity;
+                    this.log(this._hasErrors);
+                } else {
+                    if (this.humidityModifier) humidity = this.humidityModifier(humidity);
 
                     this.humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, humidity);
                     this.humidity = humidity;
                 }
-
-                callback(null, temperature);
             }
+
+            this._updating = false;
+
+            callback(null, temperature);
+
         }.bind(this));
     },
 
@@ -92,6 +145,7 @@ AdvancedHttpTemperatureHumidity.prototype = {
         temperatureService = new Service.TemperatureSensor(this.name + " Temperature");
         temperatureService
             .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({minValue: -273, maxValue: 200})
             .on('get', this.getState.bind(this));
         services.push(temperatureService);
 
